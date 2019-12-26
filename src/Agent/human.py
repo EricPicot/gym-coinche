052e636f0b57641f58a424b732b055fa7ad7981b
Eric Picot <erpicot1@airfrance.fr>
2019-12-26 11:19:41 +0000

    adding wrongly removed files

---
+++ src/Agent/human.py
@@ -0,0 +1,52 @@
+class Human:
+    def __init__(self, name, params):
+        self.name = name
+
+    def Do_Action(self, observation):
+        if observation['event_name'] == 'GameStart':
+            print(observation)
+        elif observation['event_name'] == 'NewRound':
+            print(observation)
+        elif observation['event_name'] == 'PassCards':
+            print(observation)
+            passCards = []
+            for i in range(3):
+                passCards.append(input('{0} pass card{1}: '.format(self.name, i+1)))
+
+            print('passCards: ', passCards)
+            return {
+                    "event_name" : "PassCards_Action",
+                    "data" : {
+                        'playerName': self.name,
+                        'action': {'passCards': passCards}
+                    }
+                }
+
+        elif observation['event_name'] == 'ShowPlayerHand':
+            print(observation)
+
+        elif observation['event_name'] == 'PlayTrick':
+            print(observation)
+            hand = observation['data']['hand']
+#             if '2c' in hand:
+#                 choose_card = '2c'
+#             else:
+            choose_card = input('choose card: ')
+            playtrick_action = {
+                    "event_name" : "PlayTrick_Action",
+                    "data" : {
+                        'playerName': self.name,
+                        'action': {'card': choose_card}
+                    }
+                }
+            print("playtrick_action: ", playtrick_action)
+            return playtrick_action
+
+        elif observation['event_name'] == 'ShowTrickAction':
+            print(observation)
+        elif observation['event_name'] == 'ShowTrickEnd':
+            print(observation)
+        elif observation['event_name'] == 'RoundEnd':
+            print(observation)
+        elif observation['event_name'] == 'GameOver':
+            print(observation)             
---
+++ src/run_example.py
@@ -0,0 +1,58 @@
+import gym
+
+from Coinche import *
+from Agent.human import Human
+from Agent.randomAI import RandomAI
+
+NUM_EPISODES = 1
+MAX_SCORE = 1
+
+playersNameList = ['Nord', 'Est', 'Sud', 'Ouest']
+agent_list = [0, 0, 0, 0]
+
+# Human vs Random
+
+# agent_list[0] = Human(playersNameList[0], {})
+# agent_list[1] = Human(playersNameList[1], {'print_info': True})
+# agent_list[2] = Human(playersNameList[2], {'print_info': True})
+# agent_list[3] = Human(playersNameList[3], {'print_info': True})
+
+
+# Random play
+
+agent_list[0] = Human(playersNameList[0], {'print_info': True})
+agent_list[1] = RandomAI(playersNameList[1], {'print_info': True})
+agent_list[2] = RandomAI(playersNameList[2], {'print_info': True})
+agent_list[3] = RandomAI(playersNameList[3], {'print_info': True})
+
+
+env = gym.make('Coinche_Game-v0')
+env.__init__(playersNameList, MAX_SCORE)
+
+for i_episode in range(NUM_EPISODES):
+    
+    observation = env.reset()
+    
+    while True:
+        env.render()
+
+        now_event = observation['event_name']
+        IsBroadcast = observation['broadcast']
+        action = None
+        if IsBroadcast == True:
+            for agent in agent_list:
+                agent.Do_Action(observation)
+        else:
+            playName = observation['data']['playerName']
+            for agent in agent_list:
+                if agent.name == playName:
+                    action = agent.Do_Action(observation)
+
+        observation, reward, done, info = env.step(action)
+
+        if reward != None:
+            print('\nreward: {0}\n'.format(reward))
+
+        if done:
+            print('\nGame Over!!\n')
+            break
